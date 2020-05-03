d3e9a6a0e201ef3a6d26ab12d12b6baf48ca0735
unknown <winnryan09@gmail.com>
2020-05-03 00:03:57 -0700

    test2

--- Piezas.cpp
+++ Piezas.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 /** CLASS Piezas
  * Class for representing a Piezas vertical board, which is roughly based
- * on the game "Connect Four" where pieces are placed in a column and 
+ * on the game "Connect Four" where pieces are placed in a column and
  * fall to the bottom of the column, or on top of other pieces already in
  * that column. For an illustration of the board, see:
  *  https://en.wikipedia.org/wiki/Connect_Four
@@ -17,11 +17,13 @@
 
 
 /**
- * Constructor sets an empty board (default 3 rows, 4 columns) and 
+ * Constructor sets an empty board (default 3 rows, 4 columns) and
  * specifies it is X's turn first
 **/
 Piezas::Piezas()
 {
+  turn = X;
+  board = [[Blank, Blank, Blank, Blank], [Blank, Blank, Blank, Blank],[Blank, Blank, Blank, Blank]];
 }
 
 /**
@@ -30,19 +32,33 @@ Piezas::Piezas()
 **/
 void Piezas::reset()
 {
+  board = [[Blank, Blank, Blank, Blank], [Blank, Blank, Blank, Blank],[Blank, Blank, Blank, Blank]];
 }
 
 /**
  * Places a piece of the current turn on the board, returns what
- * piece is placed, and toggles which Piece's turn it is. dropPiece does 
+ * piece is placed, and toggles which Piece's turn it is. dropPiece does
  * NOT allow to place a piece in a location where a column is full.
- * In that case, placePiece returns Piece Blank value 
+ * In that case, placePiece returns Piece Blank value
  * Out of bounds coordinates return the Piece Invalid value
  * Trying to drop a piece where it cannot be placed loses the player's turn
-**/ 
+**/
 Piece Piezas::dropPiece(int column)
 {
-    return Blank;
+  if(column > 3 || column < 0)
+  {
+    return Invalid; //Out of bounds, invalid input
+  }
+  for(int i = 2; i > -1; i--)
+  {
+    if(board[i][column] == Blank)
+    {
+      board[i][column] = turn;
+      return Blank; //Open spot found
+    }
+
+  }
+    return Invalid; //No spot found, column full
 }
 
 /**
@@ -51,13 +67,20 @@ Piece Piezas::dropPiece(int column)
 **/
 Piece Piezas::pieceAt(int row, int column)
 {
+  if(row < 0 || row > 2 || column > 3 || column < 0)
+    return Invalid; //Out of bounds
+  else if(board[row][column] == Blank)
     return Blank;
+  else if(board[row][column] == X)
+    return X;
+  else
+    return O;
 }
 
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
  * is not over, or Blank if the board is filled and no one has won ("tie").
- * For a game to be over, all locations on the board must be filled with X's 
+ * For a game to be over, all locations on the board must be filled with X's
  * and O's (i.e. no remaining Blank spaces). The winner is which player has
  * the most adjacent pieces in a single line. Lines can go either vertically
  * or horizontally. If both X's and O's have the same max number of pieces in a
@@ -65,5 +88,70 @@ Piece Piezas::pieceAt(int row, int column)
 **/
 Piece Piezas::gameState()
 {
+  /*
+  int playerX = 0;
+  int playerO = 0;
+  int maxCountR = 0;
+  int maxCountC = 0;
+
+  //Check if board is full
+  for(int i = 0; i < 2; i++)
+  {
+    for(int j = 0; j < 3; j++)
+    {
+      maxCountR = 0;
+      maxCountC = 0;
+      if(board[i][j] == Blank)
+        return Invalid;//Blank spot found, game isnt over
+      else if(board[i][j] == X || board[i][j] == O)
+      {
+        if(i == 0)
+        {
+          if(j == 0)
+          {
+            for(int k = 0; k < 2; k++)
+            {
+              if(board[k][j] != board[i][j])
+                break;
+              maxCountR++;
+            }
+            for(int l = 0; l < 3; l++)
+            {
+              if(board[i[l] != board[i][j]])
+                break;
+              maxCountC++;
+            }
+          }
+        }
+        else if(i == 1)
+        {
+
+        }
+        else
+        {
+
+        }
+      }
+
+      //Adjusts biggest X score
+      if(board[i][j] == X)
+      {
+        if(maxCountR > playerX)
+          playerX = maxCountR;
+        else if(maxCountC > playerX)
+          playerX = maxCountC;
+      }
+      //Adjusts biggest O score
+      else if(board[i][j] == O)
+      {
+        if(maxCountR > playerO)
+          playerO = maxCountR;
+        else if(maxCountC > playerO)
+          playerO = maxCountC
+      }
+    }
+  }
+  */
+
     return Blank;
 }
