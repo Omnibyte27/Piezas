15e83bc4c72d5338fb61271fe9f95d0f827825f1
unknown <winnryan09@gmail.com>
2020-05-03 12:58:57 -0700

    test

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -4,17 +4,24 @@
 
 #include <gtest/gtest.h>
 #include "Piezas.h"
- 
+
 class PiezasTest : public ::testing::Test
 {
 	protected:
 		PiezasTest(){} //constructor runs before each test
 		virtual ~PiezasTest(){} //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
 TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+TEST(PiezasTest, checkInitial)
+{
+  Piezas obj;
+  String test = obj.turn;
+  ASSERT_TRUE(test == 'X');
 }
